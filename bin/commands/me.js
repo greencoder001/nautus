efb2c0a5ffa6877e6ce52c21f555d349cc783866
Green_Lab <greencoder001@outlook.com>
2023-03-30 08:23:04 +0200

    enquirer

--- bin/commands/me.js
+++ bin/commands/me.js
@@ -5,23 +5,29 @@ module.exports = [async (args) => {
     const path = require('path')
     const ipi = require('../lib/isProjectInitialized')
     const local = args.join(' ').includes('--local')
+    const { Input, Toggle } = require('enquirer')
+    const prompt = async (msg, defaultv) => {
+        const p = new Input({
+            message: msg,
+            initial: defaultv
+        })
+        return await p.run()
+    }
     if (local && !ipi()) return console.log(chalk.red('Error: Please create a project first'))
     if (local) console.log(chalk.gray('[INFO] This will only be applied to current project'))
-    const savepath = local ? path.join(process.cwd(), 'nautus', '.nautusme') : path.join(require('os').homedir(), '.nautusme')
-
+    const savepath = local ? path.join(process.cwd(), 'nautus', '.internal', '.nautusme') : path.join(require('os').homedir(), '.nautusme')
     console.log(chalk.gray('[INFO] This will be used to complete boilerplate code. If you don\'t want to answer a question just leave it blank'))
 
-    const realName = readline.question('Real (full) name: ')
-    const githubUsername = readline.question('GitHub Username: ')
-    const name = readline.question('I like to be called: ')
-    let gender = null
-    let genderTried = false
-    while (gender !== 'male' && gender !== 'female' && gender !== '') {
-        if (genderTried) console.log(chalk.yellow('Please choose a valid option or leave blank!'))
-        genderTried = true
-        gender = readline.question('Gender (male/female): ')
-    }
-    const email = readline.questionEMail('E-Mail: ')
+    const realName = await prompt('Real (full) name')
+    const githubUsername = await prompt('GitHub Username')
+    const name = await prompt('I like to be called')
+    const gender = ((await (new Toggle({
+        message: 'Gender',
+        enabled: 'female',
+        disabled: 'male'
+    })).run()) ? 'female' : 'male')
+
+    const email = await prompt('E-Mail')
 
     const string = JSON.stringify({
         realName,
--- bin/lib/initializeProject.js
+++ bin/lib/initializeProject.js
@@ -1,32 +1,49 @@
 module.exports = async () => {
-    const { question } = require('readline-sync')
     const chalk = require('chalk')
     const fs = require('fs-extra')
     const os = require('os')
     const detectFrameworks = require('./detectFrameworks')
-    const techniques = detectFrameworks()
+    let techniques = detectFrameworks()
     const techs = {}
     const path = require('path')
     const dir = path.join(process.cwd(), 'nautus')
     const YAML = require('yaml')
+    const { MultiSelect, Input } = require('enquirer')
+    const sleep = (ms) => new Promise(r => setTimeout(r, ms))
 
     // General Info
-    const nameOfProject = question(`Project name: (${process.cwd().replace(/\\/g, '/').split('/').pop()}) `) || process.cwd().replace(/\\/g, '/').split('/').pop()
-    let projectIdentifier
+    const projectnameprompt = new Input({
+        message: 'Project name?',
+        initial: process.cwd().replace(/\\/g, '/').split('/').pop()
+    })
+    const nameOfProject = await projectnameprompt.run() || process.cwd().replace(/\\/g, '/').split('/').pop()
+    let projectIdentifier = null
     let pit = false
     while (projectIdentifier == null || projectIdentifier == '' || projectIdentifier.match(/[^\S\r\n]|:|\/|\\|[*'#²³{\[\]}=?`$!%\(\)&<>\|,;]/g)) {
+        const projectidentifierprompt = new Input({
+            message: 'Project identifier?',
+            initial: (process.cwd().replace(/\\/g, '/').split('/').pop()).replace(/[^\S\r\n]|:|\/|\\|[*'#²³{\[\]}=?`$!%\(\)&<>\|,;]/g, '')
+        })
         if (pit) console.log(chalk.red(`Project identifier cannot contain ${(projectIdentifier.match(/[^\S\r\n]|:|\/|\\|[*'#²³{\[\]}=?`$!%\(\)&<>\|,;]/g) || ['emptyness'])[0]}`))
-        projectIdentifier = question('Project identifier: ')
+        projectIdentifier = await projectidentifierprompt.run()
         pit = true
     }
 
     // Verify techniques
+    techniques = [...new Set(techniques)]
     if (techniques.length > 0) {
-        console.log('We tried to identify some used techniques for you. Please verify if you really used them.')
-        for (const tech of techniques) {
-            if (question(`${tech} (Y/n): `) !== 'n') {
-                techs[tech] = {}
-            }
+        const prompt = new MultiSelect({
+            name: 'value',
+            message: 'We tried to identify some used techniques for you. Please verify if you really used them',
+            choices: techniques.map(e => {
+                return { name: e, value: e}
+            })
+        })
+
+        const verifiedTechs = await prompt.run()
+        
+        for (const tech of verifiedTechs) {
+            techs[tech] = {}
         }
     }
 
