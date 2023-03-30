b4c1e0bc3771e1464cb09596981af6b463db47a4
Green_Lab <greencoder001@outlook.com>
2023-03-30 15:58:12 +0200

    Kelp seems to work

--- bin/lib/mergeScript.js
+++ bin/lib/mergeScript.js
@@ -2,11 +2,11 @@ const path = require('path')
 const fs = require('fs-extra')
 const chalk = require('chalk')
 
-module.exports = async (scriptName, code) => {
+module.exports = (scriptName, code) => {
     try {
         let content = fs.readFileSync(path.join(process.cwd(), 'nautus', 'scripts', `@${scriptName}.js`))
-        content = content.toString('utf-8')
-        content = content.replace(/\/\* PLEASE DON'T DELETE OR MODIFY THIS COMMENT, IT WILL BE USED TO INJECT SCRIPTS BY KELP \*\//, `\/\* PLEASE DON'T DELETE OR MODIFY THIS COMMENT, IT WILL BE USED TO INJECT SCRIPTS BY KELP \*\/
+        content = content.toString('utf8')
+        content = content.replace(/\/\* PLEASE DON'T DELETE OR MODIFY THIS COMMENT.*?\*\//, `/* PLEASE DON'T DELETE OR MODIFY THIS COMMENT, IT WILL BE USED TO INJECT SCRIPTS BY KELP */
 // Injected by kelp:
 ${code}
 `)
--- bin/lib/removeDefault.js
+++ bin/lib/removeDefault.js
@@ -2,10 +2,10 @@ const path = require('path')
 const fs = require('fs-extra')
 const chalk = require('chalk')
 
-module.exports = async (scriptName) => {
+module.exports = (scriptName) => {
     try {
         let content = fs.readFileSync(path.join(process.cwd(), 'nautus', 'scripts', `@${scriptName}.js`))
-        content = content.toString('utf-8')
+        content = content.toString('utf8')
         content = content.replace(/return error\('No (.*?) script defined, please edit \.\/nautus\/scripts\/@(\w*?)\.js'\)/, '')
         fs.writeFileSync(path.join(process.cwd(), 'nautus', 'scripts', `@${scriptName}.js`), content)
     } catch (err) {
