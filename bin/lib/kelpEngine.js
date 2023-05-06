14b11b5035b83dbd5180795b4bea42391206b44d
Green_Lab <greencoder001@outlook.com>
2023-05-06 21:54:03 +0200

    Patch #1

--- bin/lib/kelpEngine.js
+++ bin/lib/kelpEngine.js
@@ -157,8 +157,10 @@ module.exports = async (name, type = 'generator', isLocal = false) => {
         const folder = path.join(kelpDir, name)
         if (fs.existsSync(folder)) {
             // Generator available in cache
+            console.log(name)
             if (fs.existsSync(path.join(folder, 'node_modules', `nautus-${name}`, 'package.json'))) {
                 const pkgJson = fs.readJSONSync(path.join(folder, 'package.json'))
+                console.log(pkgJson.version, latest, pkgJson.version === latest)
                 if (pkgJson.version === latest) {
                     // Cached version up to date!
                 } else {
